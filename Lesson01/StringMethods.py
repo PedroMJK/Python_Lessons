7c6de175105a50de132ab2ad7a041ea30d085360
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-07-25 12:10:16 -0300

    Métodos de string adicionado

---
+++ Lesson01/StringMethods.py
@@ -0,0 +1,23 @@
+name = "Pedro";
+
+# Retorna o tamanho da da string
+print(len(name)) 
+# Encontra um caracter dentro da string solicitado
+print(name.find("P"))
+# Para captalizar a string, mas apenas a primeira palavra.
+print(name.capitalize())
+# Para colocar as letras em maiúsculas.add()
+print(name.upper())
+print(name.lower())
+# para checar se a string contem digitos
+print(name.isdigit())
+# para checar se a string contem apenas letras alfabeticas
+print(name.isalpha())
+# para contar quantos caracteres tem dentro da string.
+print(name.count("o"))
+# Para alterar um caracter por um outro
+print(name.replace("P", "C"))
+# Para multipllicar a string
+print(name*3)
+
+
