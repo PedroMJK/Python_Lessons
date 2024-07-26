5dc0c118bef73f058030986186362d12ee968e1e
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-07-26 15:18:24 -0300

    Tipos de input adicionado ao conteúdo

---
+++ Lesson01/inputTypes.py
@@ -0,0 +1,8 @@
+# Para pegar um valor dentro de uma variável
+name = input("Qual é o seu nome?: ")
+age = int(input("Quantos anos você tem?: "))
+height = float(input("Qual é a sua altura?: "))
+
+print("Olá, " + name);
+print("Você tem " +str(age) + " anos de idade.");
+print("Você tem " +str(height) + " cm de altura.");
