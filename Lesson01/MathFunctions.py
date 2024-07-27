ec7410c71b166ce1bb6d3a6f64d95b32cfde7080
Pedro Monteiro <110275925+PedroMJK@users.noreply.github.com>
2024-07-27 04:52:53 -0300

    Funções do pacote Math adicionado ao conteúdo.

---
+++ Lesson01/MathFunctions.py
@@ -0,0 +1,31 @@
+import math
+
+pi = 3.14
+
+x = 1
+y = 2
+z = 3
+
+# Para arredondar o valor
+print(round(pi))
+
+# Para arredondar o valor para cima
+print(math.ceil(pi))
+
+# Para arredondar o valor para baixo
+print(math.floor(pi))
+
+# Para dar o valor absoluto. Dando o valor que umm número está longe de zero (Se for um número negativo, ele será positivo).
+print(abs(pi))
+
+# Para elevar um número a uma potência
+print(pow(pi,2))
+
+# Para obter a raíz quadrada
+print(math.sqrt(pi))
+
+# Para encontrar o valor máximo entre variáveis
+print(max(x, y, z))
+
+# Para encontrar o valor mínimo entre variáveis
+print(min(x, y, z))
